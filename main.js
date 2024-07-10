fc4085b06949e6606d516cb6c64f15c221c1824a
wpdml <jeui4433@gmail.com>
2024-07-10 18:48:11 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 const apiKey ='4851e5992a434a07a5a77e1e205db424';
-let news = [];
+let news = []
 
 const getNews = async ()=>{
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`);
-    const response = await fetch(url);
-    const data = await response.json();
-    news = data.articles
-    console.log("ddd",news);
+    const url = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?apiKey=${apiKey}&page=1&pageSize=10`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        news = data.articles;
+        displayNews();
+    } catch (error) {
+        console.error('Fetch error: ', error);
+    }
 };
+
+const displayNews = () => {
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = news.map(article => `
+        <div class="news-article">
+            <h2>${article.title}</h2>
+            <p>${article.description}</p>
+            <a href="${article.url}" target="_blank">Read more</a>
+        </div>
+    `).join('');
+};
+
 getNews();
 
